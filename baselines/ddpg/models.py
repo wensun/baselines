d1b3f42bdec5493e0029226cb0fb3699e9a54120
Wen Sun <wensun@Wens-MBP.fios-router.home>
2018-04-06 00:53:46 -0400

    add linear actor and quadratic critic

--- baselines/ddpg/models.py
+++ baselines/ddpg/models.py
@@ -1,6 +1,15 @@
 import tensorflow as tf
 import tensorflow.contrib as tc
 
+#implementation of quadratic feature mapping for a batch of input X
+def tf_quadratic_mapping(symbolic_x):
+    #x shape: (None, d)
+    d = symbolic_x.get_shape().as_list()[-1]
+    x1 = tf.expand_dims(symbolic_x, 2) # (None, d, 1)
+    x2 = tf.expand_dims(symbolic_x, 1) # (None, 1, d)
+    row_wise_cross_prod = tf.matmul(x1, x2) #(None, d,d)
+    return tf.reshape(row_wise_cross_prod, shape = [-1, d*d])
+    
 
 class Model(object):
     def __init__(self, name):
@@ -46,6 +55,23 @@ class Actor(Model):
         return x
 
 
+class Linear_Actor(Model):
+    def __init__(self, nb_actions, name='actor', layer_norm=True):
+        super(Actor, self).__init__(name=name)
+        self.nb_actions = nb_actions
+        self.layer_norm = layer_norm
+    
+    def __call__(self, obs, reuse = False):
+        with tf.variable_scope(self.name) as scope:
+            if reuse:
+                scope.reuse_variables()
+            
+            x = obs
+            x = tf.layers.dense(x, self.nb_actions, kernel_initializer=tf.random_uniform_initializer(minval=-3e-3, maxval=3e-3))
+        return x
+
+
+
 class Critic(Model):
     def __init__(self, name='critic', layer_norm=True):
         super(Critic, self).__init__(name=name)
@@ -68,6 +94,27 @@ class Critic(Model):
                 x = tc.layers.layer_norm(x, center=True, scale=True)
             x = tf.nn.relu(x)
 
+            x = tf.layers.dense(x, 1, kernel_initializer=tf.random_uniform_initializer(minval=-3e-3, maxval=3e-3))
+        return x
+
+    @property
+    def output_vars(self):
+        output_vars = [var for var in self.trainable_vars if 'output' in var.name]
+        return output_vars
+
+class Quadratic_Critic(Model):
+    def __init__(self, name='critic', layer_norm=True):
+        super(Critic, self).__init__(name=name)
+        self.layer_norm = layer_norm
+    
+    def __call__(self, obs, action, reuse=False):
+        with tf.variable_scope(self.name) as scope:
+            if reuse:
+                scope.reuse_variables()
+        
+            x = obs
+            #create quadratic feature:
+            x = tf_quadratic_mapping(x)
             x = tf.layers.dense(x, 1, kernel_initializer=tf.random_uniform_initializer(minval=-3e-3, maxval=3e-3))
         return x
 
